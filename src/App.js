0728807c5f29bfd99aac10e638786f4a4a146f12
EleazarFederio <eleazarfederio_@outlook.com>
2020-12-10 00:24:18 +0800

    add filter component to redux

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import Cart from './components/Cart';
 import Filter from './components/Filter';
 import Products from './components/Products';
-import data from './data.json';
 import { store } from './store';
 
 class App extends Component {
@@ -11,10 +10,7 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      products: data.products,
       cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
-      size: "",
-      sort: "",
     }
   }
 
@@ -51,43 +47,6 @@ class App extends Component {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }
 
-  sortProducts = (event) => {
-    const sort = event.target.value;
-    console.log(event.target.value);
-    this.setState((state) => ({
-      sort: sort,
-      products: this.state.products
-        .slice()
-        .sort((a, b) =>
-          sort === "lowest"
-            ? a.price > b.price
-              ? 1
-              : -1
-            : sort === "highest"
-            ? a.price < b.price
-              ? 1
-              : -1
-            : a._id < b._id
-            ? 1
-            : -1
-        ),
-    }));
-  }
-
-  filterProducts = (event) => {
-    console.log(event.target.value);
-    if (event.target.value === "") {
-      this.setState({ size: event.target.value, products: data.products });
-    } else {
-      this.setState({
-        size: event.target.value,
-        products: data.products.filter(
-          (product) => product.availableSizes.indexOf(event.target.value) >= 0
-        ),
-      });
-    }
-  }
-
   render(){
     return (
       <Provider store={store}>
@@ -98,8 +57,8 @@ class App extends Component {
           <main>
             <div className={'content'}>
               <div className={'main'}>
-                <Filter count={this.state.products.length} size={this.state.size} sort={this.state.sort} filterProducts={this.filterProducts} sortProducts={this.sortProducts} />
-                <Products products={this.state.products} addToCart={this.addToCart} />
+                <Filter />
+                <Products />
               </div>
               <div className={'sidebar'}> 
                 <Cart cartItems={this.state.cartItems} createOrder={this.createOrder} removeFromCart={this.removeFromCart} />
--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,6 +1,7 @@
 
 import {axios} from "../lib/axios";
-import { FETCH_PRODUCTS } from "../types";
+import { FETCH_PRODUCTS, FILTER_PRODUCTS_BY_SIZE, ORDER_PRODUCTS_BY_PRICE } from "../types";
+
 
 export const fetchProducts = () => async(dispatch) => {
     const response = await axios.get('/products').
@@ -11,5 +12,43 @@ export const fetchProducts = () => async(dispatch) => {
     dispatch({
         type: FETCH_PRODUCTS,
         payload: response.data.data,
+    })
+}
+
+export const filterProducts = (products, size) => (dispatch) => {
+    dispatch({
+        type: FILTER_PRODUCTS_BY_SIZE,
+        payload: {
+            size: size,
+            items: size === "" ? products:
+            products.filter((x) => x.availableSizes.indexOf(size) >= 0)
+        },
+    });
+};
+
+export const sortProducts = (filteredProducts, sort) => (dispatch) => {
+    const sortedProducts = filteredProducts.slice();
+    // if empty sort by id
+    if(sort === "latest"){
+        sortedProducts.sort((a, b) => (a._id > b._id ? 1 : -1));
+    }else{
+        sortedProducts.sort((a, b) => (
+            sort === "lowest" ? 
+            // lowest to highest
+            a.price > b.price ? 1 : -1
+            :
+            // highest to lowest
+            a.price > b.price ? -1 : 1
+        ))
+    }
+    console.log('----------------------------------------')
+    console.log(sortProducts)
+
+    dispatch({
+        type: ORDER_PRODUCTS_BY_PRICE,
+        payload: {
+            sort: sort,
+            items: sortedProducts
+        }
     })
 }
